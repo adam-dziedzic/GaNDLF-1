31f0c021a8c58c5598bb9a7abdb6ca71916cdb8b
Edwards <edwardsb@spr-gpu01.jf.intel.com>
2022-01-22 14:15:07 -0800

    included data shape info into config for dp tests

--- testing/test_full.py
+++ testing/test_full.py
@@ -1130,6 +1130,8 @@ def test_differential_privacy_simple_classification_rad_2d(device):
     parameters["model"]["num_channels"] = 3
     parameters["model"]["norm_type"] = "instance"
     parameters["differential_privacy"] = True
+    parameters["data"] = {"feature_shape": [3, 128, 128, 1], 
+                          "label_shape": [3, 128, 128, 1]}
     file_config_temp = os.path.join(testingDir, "config_segmentation_temp.yaml")
     # if found in previous run, discard.
     if os.path.exists(file_config_temp):
@@ -1171,6 +1173,8 @@ def test_differential_privacy_epsilon_classification_rad_2d(device):
     parameters["model"]["num_channels"] = 3
     parameters["model"]["norm_type"] = "instance"
     parameters["differential_privacy"] = {"epsilon": 25.0}
+    parameters["data"] = {"feature_shape": [3, 128, 128, 1], 
+                          "label_shape": [3, 128, 128, 1]}
     file_config_temp = os.path.join(testingDir, "config_segmentation_temp.yaml")
     # if found in previous run, discard.
     if os.path.exists(file_config_temp):
